6929116a2ca428bd8097089e8ee5ce7f6dc63097
João Rafael Campos <65449851+jocamposdot@users.noreply.github.com>
2020-11-27 14:22:23 -0300

    Add files via upload

---
+++ produtos-apirest-master/src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -0,0 +1,39 @@
+package com.produtos.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api")
+public class ProdutoResource {
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	@GetMapping("/produto/{id}")
+	public Produto buscaProdutoUnico(@PathVariable("id") long id) {
+		return produtoRepository.findById(id);
+	}
+
+	@PostMapping("/produto/add")
+	public Produto insereProduto(@RequestBody Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+		
+	}
+}
